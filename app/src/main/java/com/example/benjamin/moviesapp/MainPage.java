a082084b070718c52fa5856e76ceb25e70f33ace
BenjaminLeg <legrosbenjamin@yahoo.fr>
2018-10-12 09:19:09 +0200

    Correcting conflicts

--- app/src/main/java/com/example/benjamin/moviesapp/MainPage.java
+++ app/src/main/java/com/example/benjamin/moviesapp/MainPage.java
@@ -5,24 +5,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainPage extends AppCompatActivity implements OnLoadingListener {
     public static Context context;
@@ -30,6 +39,7 @@ public class MainPage extends AppCompatActivity implements OnLoadingListener {
 
     List<Movie> moviesList;
     RecyclerView recyclerView;
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -100,11 +110,9 @@ public class MainPage extends AppCompatActivity implements OnLoadingListener {
         );
 
 
-        moviesList=new ArrayList<Movie>();
+        moviesList = new ArrayList<Movie>();
         mMovieParser = new GetMoviesParseTask(this, moviesList);
         mMovieParser.execute();
-
-
 
 
     }
@@ -121,43 +129,29 @@ public class MainPage extends AppCompatActivity implements OnLoadingListener {
     }
 
 
-
-
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        Button share = findViewById(R.id.shareButton);
-        FloatingActionButton addBtn;
+      /*  Button share = findViewById(R.id.shareButton);
+        FloatingActionButton addBtn = findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String filename="favoris";
-                Movie content=new Movie;
+                String filename = "favoris";
+             //   Movie content = new Movie();
                 FileOutputStream stream;
-                try{
+                try {
                     stream = v.getContext().openFileOutput(filename, Context.MODE_PRIVATE);
-                    stream.write(content.getBytes());
+                    // stream.write(content.getBytes());
                     stream.close();
-                    Log.e("DebugPerso","Film ajouté");
+                    Log.e("DebugPerso", "Film ajouté");
 
-                }catch (Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        });
-        /*share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //sendSms();
-                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }*/
-
-   /* private void sendSms() {
-        Intent share = new Intent(this, SendSms.class);
-    }*/
+        });*/
+    }
 
     @Override
     public void loadChange(Void success) {
@@ -166,8 +160,6 @@ public class MainPage extends AppCompatActivity implements OnLoadingListener {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(new MoviesAdaptator(moviesList));
-
-
+        
     }
-
 }
