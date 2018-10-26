f994ab558b778d4ef9321a9f6b2cddc96b95e211
= <=>
2018-10-26 18:22:12 +0200

    Test Media

---
+++ app/src/main/java/com/example/benjamin/moviesapp/activities/MySessionCalback.java
@@ -0,0 +1,22 @@
+package com.example.benjamin.moviesapp.activities;
+
+import android.support.v4.media.session.MediaSessionCompat;
+
+class MySessionCalback extends MediaSessionCompat.Callback {
+    @Override
+    public void onPlay() {
+        super.onPlay();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+}
